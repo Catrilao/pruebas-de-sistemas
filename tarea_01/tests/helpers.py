445486a65c29b76b327d62d9a1c65d6c0395f002
Catrilao <angel.lopez@alumnos.uv.cl>
2024-10-03 22:43:21 -0300

    Agregar función helper de distancia

---
+++ tarea_01/tests/helpers.py
@@ -0,0 +1,43 @@
+import requests
+import json
+
+
+def distance(source, destination, unit):
+    begin = {
+        "geometryType": "esriGeometryPoint",
+        "geometry": {
+            "x": source[0],
+            "y": source[1],
+        },
+    }
+
+    end = {
+        "geometryType": "esriGeometryPoint",
+        "geometry": {
+            "x": destination[0],
+            "y": destination[1],
+        },
+    }
+
+    url = "https://sampleserver6.arcgisonline.com/arcgis/rest/services/Utilities/Geometry/GeometryServer/distance"
+    params = {
+        "sr": 4326,
+        "distanceUnit": unit,
+        "geometry1": json.dumps(begin),
+        "geometry2": json.dumps(end),
+        "geodesic": True,
+        "f": "pjson",
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    return data
+
+
+if __name__ == "__main__":
+    distance(
+        source=(-91, 30),
+        destination=(40, 30),
+        unit="9036",
+    )
--- tarea_01/tests/tests_cliente.py
+++ tarea_01/tests/tests_cliente.py
@@ -1,12 +1,7 @@
 import unittest
-from unittest.mock import Mock, patch, call
+from unittest.mock import patch
 
-from src  import distance_client
-from src import distance_grpc_service
-from src import distance_unary_pb2 as pb2
-from src.distance_grpc_service import DistanceServicer
-
-
+from src import distance_client
 
 
 class TestCliente(unittest.TestCase):
@@ -33,9 +28,8 @@ class TestCliente(unittest.TestCase):
         del cls.unidad_invalida
         del cls.res_distancia_invalida
 
-    
     def test_latitud_no_valida(cls):
-        with patch('builtins.print') as mocked_print:
+        with patch("builtins.print") as mocked_print:
             distance_client.main(
                 source=(cls.latitud_no_valida, cls.longitud_valida),
                 destination=(cls.latitud_valida, cls.longitud_valida),
@@ -45,20 +39,20 @@ class TestCliente(unittest.TestCase):
             mocked_print.assert_any_call("Distance:", cls.res_distancia_invalida)
             mocked_print.assert_any_call("Distance unit:", cls.unidad_invalida)
 
-# verificar que la respuesta coincide con km
+    # verificar que la respuesta coincide con km
     def test_respuesta_en_km(cls):
-            with patch('builtins.print') as mocked_print:
-                distance_client.main(
-                    source=(cls.latitud_valida, cls.longitud_valida),
-                    destination=(cls.latitud_valida + 10, cls.longitud_valida + 10),
-                    unit=cls.unidad_km,
-                )
-                # Verificar que la respuesta contiene la unidad "km"
-                mocked_print.assert_any_call("Distance unit:", cls.unidad_km)
+        with patch("builtins.print") as mocked_print:
+            distance_client.main(
+                source=(cls.latitud_valida, cls.longitud_valida),
+                destination=(cls.latitud_valida + 10, cls.longitud_valida + 10),
+                unit=cls.unidad_km,
+            )
+            # Verificar que la respuesta contiene la unidad "km"
+            mocked_print.assert_any_call("Distance unit:", cls.unidad_km)
 
-# verificar que la respuesta coincide con nm
+    # verificar que la respuesta coincide con nm
     def test_respuesta_en_nm(cls):
-        with patch('builtins.print') as mocked_print:
+        with patch("builtins.print") as mocked_print:
             distance_client.main(
                 source=(cls.latitud_valida, cls.longitud_valida),
                 destination=(cls.latitud_valida + 10, cls.longitud_valida + 10),
@@ -67,18 +61,16 @@ class TestCliente(unittest.TestCase):
             # Verificar que la respuesta contiene la unidad "nm"
             mocked_print.assert_any_call("Distance unit:", cls.unidad_nm)
 
-#verificar que la respuesta sea km cuando  esté en blanco
+    # verificar que la respuesta sea km cuando  esté en blanco
     def test_respuesta_predeterminada_km(cls):
-        with patch('builtins.print') as mocked_print:
+        with patch("builtins.print") as mocked_print:
             distance_client.main(
                 source=(cls.latitud_valida, cls.longitud_valida),
                 destination=(cls.latitud_valida + 10, cls.longitud_valida + 10),
                 unit="",  # No se especifica la unidad
             )
             mocked_print.assert_any_call("Distance unit:", cls.unidad_km)
-    
+
+
 if __name__ == "__main__":
     unittest.main()
-
-
-
